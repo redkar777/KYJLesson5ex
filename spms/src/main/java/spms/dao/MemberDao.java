a06ff0e51fd8e6d47ae28efc1df5eabfcc35df4d
redkar777 <redkar777@naver.com>
2016-12-16 16:48:16 +0900

    글입력

--- spms/src/main/java/spms/dao/MemberDao.java
+++ spms/src/main/java/spms/dao/MemberDao.java
@@ -1,6 +1,7 @@
 package spms.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -15,10 +16,34 @@ public class MemberDao {
 	public void setConnection(Connection connection) {
 		this.connection = connection;
 	}
+	public int MemeberInsert(Member member) throws Exception{
+		PreparedStatement stmt = null;
+		int memberCount = 0;
+		try{
+			
+			
+			stmt = connection.prepareStatement(
+					"INSERT INTO MEMBERS(EMAIL,PWD,MNAME,CRE_DATE,MOD_DATE)"
+					+ " VALUES (?,?,?,NOW(),NOW())");
+			stmt.setString(1, member.getEmail());
+			stmt.setString(2, member.getPassword());
+			stmt.setString(3, member.getName());
+			memberCount = stmt.executeUpdate();
+			
+			return memberCount;
+		} catch(Exception e) {
+			throw e;
+		} finally{
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+		}
+		
+		
+		
+	}
 	public List<Member> selectList() throws Exception{
 		Statement stmt = null;
 		ResultSet rs = null;
-
+		
 		try {
 			stmt = connection.createStatement();
 			rs = stmt.executeQuery(
@@ -35,6 +60,7 @@ public class MemberDao {
 							.setEmail(rs.getString("EMAIL"))
 							.setCreatedDate(rs.getDate("CRE_DATE"))	);
 			}
+			return members;	
 						
 		} catch(Exception e){
 			throw e;
@@ -44,6 +70,6 @@ public class MemberDao {
 		}
 			
 		
-		return null;		
+			
 	}
 }
--- spms/src/main/java/spms/servlets/MemberAddServlet.java
+++ spms/src/main/java/spms/servlets/MemberAddServlet.java
@@ -12,6 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import spms.dao.MemberDao;
+import spms.vo.Member;
+
 // JSP 적용
 // - 입력폼 및 오류 처리 
 @WebServlet("/member/add")
@@ -37,13 +40,19 @@ public class MemberAddServlet extends HttpServlet {
 		try {
 			ServletContext sc = this.getServletContext();
 			conn = (Connection) sc.getAttribute("conn");  
-			stmt = conn.prepareStatement(
-					"INSERT INTO MEMBERS(EMAIL,PWD,MNAME,CRE_DATE,MOD_DATE)"
-					+ " VALUES (?,?,?,NOW(),NOW())");
-			stmt.setString(1, request.getParameter("email"));
-			stmt.setString(2, request.getParameter("password"));
-			stmt.setString(3, request.getParameter("name"));
-			stmt.executeUpdate();
+			
+			MemberDao memberDao = new MemberDao();
+			memberDao.setConnection(conn);
+			String memberEmail = request.getParameter("email");
+			String memberPassword = request.getParameter("password");
+			String memberName = request.getParameter("name");
+			
+			Member member = new Member();
+			member.setEmail(memberEmail);
+			member.setPassword(memberPassword);
+			member.setName(memberName);
+			
+			int memberCount = memberDao.MemeberInsert(member);
 			
 			response.sendRedirect("list");
 			
@@ -53,9 +62,6 @@ public class MemberAddServlet extends HttpServlet {
 			RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
 			rd.forward(request, response);
 			
-		} finally {
-			try {if (stmt != null) stmt.close();} catch(Exception e) {}
-			//try {if (conn != null) conn.close();} catch(Exception e) {}
-		}
+		} 
 	}
 }
